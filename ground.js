eadcca521fd7ae0cd8616fbdb1e044abd8817aa3
Moad Hamousti <118165767+moadhamousti@users.noreply.github.com>
2023-10-31 20:17:33 +0100

    Update ground.js

--- ground.js
+++ ground.js
@@ -2,7 +2,7 @@ import {
   getCustomProperty,
   incrementCustomProperty,
   setCustomProperty,
-} from "./updateCustomProperty.js"
+} from "./updateCustomProperty.js" 
  
 const SPEED = 0.05
 const groundElems = document.querySelectorAll("[data-ground]")
