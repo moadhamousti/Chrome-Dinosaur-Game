3a883493c91c6b099bebf8bd714e8edad9e27c8f
Moad Hamousti <118165767+moadhamousti@users.noreply.github.com>
2023-11-16 15:17:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { updateGround, setupGround } from "./ground.js"
 import { updateDino, setupDino, getDinoRect, setDinoLose } from "./dino.js"
 import { updateCactus, setupCactus, getCactusRects } from "./cactus.js"
-
+ 
 const WORLD_WIDTH = 100
 const WORLD_HEIGHT = 30
 const SPEED_SCALE_INCREASE = 0.00001
